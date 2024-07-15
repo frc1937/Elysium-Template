ffbd45a00773f64a7748bfb38190f8513c83bd1e
itsWihy <devwihy@gmail.com>
2024-07-15 22:11:14 +0300

    Refactor: Implement different intake

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,13 +2,12 @@ package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.littletonrobotics.junction.Logger;
+import frc.lib.generic.GenericSubsystem;
+import frc.lib.generic.motor.MotorProperties;
 
-public class Intake extends SubsystemBase {
-    private final IntakeInputsAutoLogged inputs = new IntakeInputsAutoLogged();
-    private final IntakeIO intakeIO = IntakeIO.generateIntake();
+import static frc.robot.subsystems.intake.IntakeConstants.INTAKE_MOTOR;
 
+public class Intake extends GenericSubsystem {
     public Command setIntakeSpeed(double speed) {
         return Commands.startEnd(
                 () -> setPercentageOutput(speed),
@@ -17,17 +16,15 @@ public class Intake extends SubsystemBase {
         );
     }
 
+    public Command stopIntake() {
+        return Commands.runOnce(this::stop, this);
+    }
+
+    private void setPercentageOutput(double percentageOutput) {
+        INTAKE_MOTOR.setOutput(MotorProperties.ControlMode.PERCENTAGE_OUTPUT, percentageOutput);
+    }
+
     private void stop() {
-        intakeIO.stop();
-    }
-
-    private void setPercentageOutput(double speed) {
-        intakeIO.setPercentageOutput(speed);
-    }
-
-    @Override
-    public void periodic() {
-        intakeIO.refreshInputs(inputs);
-        Logger.processInputs("Intake", inputs);
+        INTAKE_MOTOR.stopMotor();
     }
 }
