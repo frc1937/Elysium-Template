ddc0cf3cb833b3e5ea6585ef195a3183b3de623a
Wihy <105944317+itsWihy@users.noreply.github.com>
2024-10-29 08:41:40 +0200

    Feat: enhanced rio logging when out of space

--- src/main/java/frc/lib/generic/hardware/HardwareManager.java
+++ src/main/java/frc/lib/generic/hardware/HardwareManager.java
@@ -1,6 +1,5 @@
 package frc.lib.generic.hardware;
 
-import edu.wpi.first.wpilibj.Filesystem;
 import frc.lib.generic.OdometryThread;
 import frc.lib.generic.advantagekit.LoggableHardware;
 import frc.lib.generic.simulation.GenericSimulation;
@@ -12,17 +11,26 @@ import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static frc.robot.GlobalConstants.*;
+import static frc.robot.GlobalConstants.CURRENT_MODE;
+import static frc.robot.GlobalConstants.FASTER_THREAD_LOCK;
+import static frc.robot.GlobalConstants.SHOULD_WRITE_LOGS;
 
 //Credit to team 418 for this
 public enum HardwareManager {
     INSTANCE;
 
-    HardwareManager() {}
+    private static final boolean IS_PRACTICE = true;
+    private static final long MIN_FREE_SPACE = IS_PRACTICE ? 100000000 /*100 MB*/: 1000000000 /*1 GB*/;
+
+    HardwareManager() {
+    }
 
     private static final List<LoggableHardware> hardware = new ArrayList<>();
     private static final List<Runnable> periodicRunnable = new ArrayList<>();
@@ -37,7 +45,14 @@ public enum HardwareManager {
      * @param robot Robot object
      */
     public static void initialize(LoggedRobot robot) {
-        String logPath = Filesystem.getDeployDirectory().getPath() + "/logs/";
+        String logPath = "/home/lvuser/logs";
+
+        final File logsDirectory = new File(logPath);
+
+        if (!logsDirectory.exists())
+            logsDirectory.mkdir();
+
+        cleanOldFiles(logsDirectory);
 
         if (CURRENT_MODE == GlobalConstants.Mode.REAL || CURRENT_MODE == GlobalConstants.Mode.SIMULATION) {
             Logger.addDataReceiver(new NT4Publisher());
@@ -100,5 +115,43 @@ public enum HardwareManager {
 
     public static void updateSimulation() {
         GenericSimulation.updateAllSimulations();
+    }
+
+    private static void cleanOldFiles(File logsDirectory) {
+        if (!SHOULD_WRITE_LOGS || logsDirectory.getFreeSpace() >= MIN_FREE_SPACE)
+            return;
+
+        System.out.println("[!] ERROR: out of space!");
+        File[] files = logsDirectory.listFiles();
+
+        if (files == null) {
+            System.out.println("[!] ERROR: Cannot delete, Files are NULL!");
+            return;
+        }
+
+        // Sorting the files by name will ensure that the oldest files are deleted first
+        files = Arrays.stream(files).sorted().toArray(File[]::new);
+
+        long bytesToDelete = MIN_FREE_SPACE - logsDirectory.getFreeSpace();
+
+        for (File file : files) {
+            if (!file.getName().endsWith(".wpilog")) continue;
+
+            try {
+                bytesToDelete -= Files.size(file.toPath());
+            } catch (IOException e) {
+                System.out.println("[!] Failed to get size of file " + file.getName());
+                continue;
+            }
+
+            if (file.delete()) {
+                System.out.println("[!] Deleted " + file.getName() + " to free up space");
+            } else {
+                System.out.println("[!] Failed to delete " + file.getName());
+            }
+
+            if (bytesToDelete <= 0)
+                return;
+        }
     }
 }
