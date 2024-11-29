608c05a370c695063deaf3573bb08f79507a6039
Wihy <105944317+itsWihy@users.noreply.github.com>
2024-11-29 11:41:46 +0200

    Feat: add a lookup table for ARM shooting

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.arm;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
@@ -12,6 +13,8 @@ import frc.lib.math.Conversions;
 import frc.robot.subsystems.flywheels.FlywheelsConstants;
 import org.littletonrobotics.junction.Logger;
 
+import java.util.function.DoubleSupplier;
+
 import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.Volts;
@@ -20,6 +23,7 @@ import static frc.robot.subsystems.arm.ArmConstants.ARM_MECHANISM;
 import static frc.robot.subsystems.arm.ArmConstants.ARM_MOTOR;
 import static frc.robot.subsystems.arm.ArmConstants.SYSID_CONFIG;
 import static frc.robot.utilities.ShooterPhysicsCalculations.calculateShootingAngle;
+import static frc.robot.utilities.ShooterPhysicsCalculations.zoneInOnShootingAngle;
 
 public class Arm extends GenericSubsystem {
     public Arm() {
@@ -37,7 +41,7 @@ public class Arm extends GenericSubsystem {
         return ARM_MOTOR.getSystemVelocity();
     }
 
-    public Command setTargetPhysicsBasedPosition(Pose3d targetPose, double targetVelocityRPS) {
+    public Command setTargetPhysicsBasedPositionNoZoneIn(Pose3d targetPose, double targetVelocityRPS) {
         double[] targetAngleRotations = new double[1];
 
         int[] i = {0};
@@ -48,19 +52,39 @@ public class Arm extends GenericSubsystem {
                             calculateShootingAngle(targetPose, Conversions.rpsToMps(targetVelocityRPS, FlywheelsConstants.RIGHT_FLYWHEEL_DIAMETER)).getRotations();
                 },
                 () -> {
-                    i[0] = i[0]+1;
-
-                    if (i[0] < 5) {
-                        targetAngleRotations[0] =
-                                calculateShootingAngle(targetPose,
-                                        Conversions.rpsToMps(targetVelocityRPS,
-                                                FlywheelsConstants.RIGHT_FLYWHEEL_DIAMETER))
-                                        .getRotations();
-                    }
+//                    i[0] = i[0]+1;
+//
+//                    if (i[0] < 5) {
+//                        targetAngleRotations[0] =
+//                                calculateShootingAngle(targetPose,
+//                                        Conversions.rpsToMps(targetVelocityRPS,
+//                                                FlywheelsConstants.RIGHT_FLYWHEEL_DIAMETER))
+//                                        .getRotations();
+//                    }
 
                     setMotorTargetPosition(Rotation2d.fromRotations(targetAngleRotations[0]));
 
-                    i[0] %= 50; //Divide by seconds. Each second, refresh and recalc position!
+//                    i[0] %= 30; //Divide by seconds. Each second, refresh and recalc position!
+                },
+                interrupted -> ARM_MOTOR.stopMotor(),
+                () -> false,
+                this
+        );
+    }
+
+
+    public Command setTargetPhysicsBasedPosition(Pose3d targetPose, double targetVelocityRPS) {
+        double[] targetAngleRotations = new double[1];
+
+//        int[] i = {0};
+
+        return new FunctionalCommand(
+                () -> {
+                    targetAngleRotations[0] =
+                            zoneInOnShootingAngle(targetPose, Conversions.rpsToMps(targetVelocityRPS, FlywheelsConstants.RIGHT_FLYWHEEL_DIAMETER)).getRotations();
+                },
+                () -> {
+                    setMotorTargetPosition(Rotation2d.fromRotations(targetAngleRotations[0] + Units.degreesToRotations(3)));
                 },
                 interrupted -> ARM_MOTOR.stopMotor(),
                 () -> false,
@@ -70,13 +94,26 @@ public class Arm extends GenericSubsystem {
 
     public Command setTargetPosition(Rotation2d targetPosition) {
         return new FunctionalCommand(
-                () -> {},
+                () -> {
+                },
                 () -> setMotorTargetPosition(targetPosition),
                 interrupted -> ARM_MOTOR.stopMotor(),
                 () -> false,
                 this
         );
     }
+
+    public Command setTargetPositionSupplier(DoubleSupplier targetPosition) {
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> setMotorTargetPosition(Rotation2d.fromDegrees(targetPosition.getAsDouble())),
+                interrupted -> ARM_MOTOR.stopMotor(),
+                () -> false,
+                this
+        );
+    }
+
 
     @Override
     public void periodic() {
@@ -114,7 +151,7 @@ public class Arm extends GenericSubsystem {
     }
 
     private void setMotorTargetPosition(Rotation2d targetPosition) {
-        ARM_MOTOR.setOutput(MotorProperties.ControlMode.POSITION,  targetPosition.getRotations());
+        ARM_MOTOR.setOutput(MotorProperties.ControlMode.POSITION, targetPosition.getRotations());
         ARM_MECHANISM.setTargetAngle(targetPosition);
     }
 }
--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.arm;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.util.Color;
@@ -17,6 +18,7 @@ import frc.lib.generic.hardware.motor.MotorFactory;
 import frc.lib.generic.hardware.motor.MotorProperties;
 import frc.lib.generic.simulation.SimulationProperties;
 import frc.lib.generic.simulation.mechanisms.SingleJointedArmMechanism2d;
+import frc.lib.util.LoggedTunableNumber;
 
 import static edu.wpi.first.units.Units.Second;
 import static edu.wpi.first.units.Units.Seconds;
@@ -46,9 +48,41 @@ public class ArmConstants {
 
     static final Rotation2d PIVOT_ENCODER_OFFSET = Rotation2d.fromDegrees(21.478516);
 
+    public static final LoggedTunableNumber CURRENT_ANGLE_CALIB = new LoggedTunableNumber("CalibratedShootingAngle", 65);
+    public static final InterpolatingDoubleTreeMap SHOOTING_ANGLES = new InterpolatingDoubleTreeMap();
+
     static {
         configureMotor();
         configureEncoder();
+        configureShootingAngles();
+    }
+
+    private static void configureShootingAngles() {
+        SHOOTING_ANGLES.put(0.0, 57.295);
+        SHOOTING_ANGLES.put(0.2, 57.295);
+        SHOOTING_ANGLES.put(0.4, 57.281);
+        SHOOTING_ANGLES.put(0.6, 57.012);
+        SHOOTING_ANGLES.put(0.8, 56.036);
+        SHOOTING_ANGLES.put(1.0, 54.247);
+        SHOOTING_ANGLES.put(1.2, 51.861);
+        SHOOTING_ANGLES.put(1.4, 49.159);
+        SHOOTING_ANGLES.put(1.6, 46.369);
+        SHOOTING_ANGLES.put(1.8, 43.636);
+        SHOOTING_ANGLES.put(2.0, 41.04);
+        SHOOTING_ANGLES.put(2.2, 38.621);
+        SHOOTING_ANGLES.put(2.4, 36.391);
+        SHOOTING_ANGLES.put(2.6, 34.346);
+        SHOOTING_ANGLES.put(2.8, 32.477);
+        SHOOTING_ANGLES.put(3.0, 30.77);
+        SHOOTING_ANGLES.put(3.2, 29.211);
+        SHOOTING_ANGLES.put(3.4, 27.784);
+        SHOOTING_ANGLES.put(3.6, 26.477);
+        SHOOTING_ANGLES.put(3.8, 25.277);
+        SHOOTING_ANGLES.put(4.0, 24.173);
+        SHOOTING_ANGLES.put(4.2, 23.154);
+        SHOOTING_ANGLES.put(4.4, 22.213);
+        SHOOTING_ANGLES.put(4.6, 21.341);
+        SHOOTING_ANGLES.put(4.8, 20.532);
     }
 
     private static void configureEncoder() {
@@ -75,9 +109,9 @@ public class ArmConstants {
         motorConfiguration.idleMode = MotorProperties.IdleMode.BRAKE;
         motorConfiguration.supplyCurrentLimit = 40;
 
-        motorConfiguration.profiledMaxVelocity = 0.5;
-        motorConfiguration.profiledTargetAcceleration = 0.35;
-        motorConfiguration.profiledJerk = 3.5;
+        motorConfiguration.profiledMaxVelocity = 0.6;
+        motorConfiguration.profiledTargetAcceleration = 0.4;
+        motorConfiguration.profiledJerk = 4;
 
         motorConfiguration.closedLoopTolerance = TOLERANCE_ROTATIONS;
 
